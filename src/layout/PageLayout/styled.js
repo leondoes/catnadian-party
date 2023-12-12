38723a57173d6bf4f1e9fddfcba82b6b6b27df55
leondoes <leon.dobrescu@gmail.com>
2023-12-12 02:47:17 -0500

    :art: default background color set

--- src/layout/PageLayout/styled.js
+++ src/layout/PageLayout/styled.js
@@ -2,9 +2,7 @@ import styled from "styled-components";
 
 export const PageContainer = styled.div`
   padding-top: 40px;
-  background-color: #e2d8ce;
   
-
   //Doesn't move content on webpage when there's no scroll bar
   padding-left: calc(100vw - 100%);
 
@@ -38,7 +36,6 @@ export const PageContainer = styled.div`
 
 export const ContentContainer = styled.div`
 display: flex;
-height: 100vh;
 margin: 30px 100px 30px 100px;
 
   @keyframes slideInFromBottom {
