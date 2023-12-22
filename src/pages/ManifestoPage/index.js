219de11d5284b3cddf5707796f268beffd37449e
leondoes <leon.dobrescu@gmail.com>
2023-12-22 03:52:28 -0500

    :sparkles: Added manifesto page

--- src/pages/ManifestoPage/index.js
+++ src/pages/ManifestoPage/index.js
@@ -3,3 +3,24 @@ Manifesto Component: Detailed parody policies and party promises
 (e.g., more napping spots, unlimited catnip, etc.).
 FAQ Component: Answering humorous frequently asked questions about the party.
 */
+
+import React from 'react';
+
+const ManifestoComponent = () => {
+    return (
+        <div className="manifesto">
+            <h1>Catnadian Political Party Manifesto</h1>
+            <p>Welcome to the purr-fect vision of the future! Our Catnadian Political Party stands for the following delightful decrees:</p>
+            <ul>
+                <li><strong>More Napping Spots:</strong> Every public space will have cozy sunlit spots designated for napping.</li>
+                <li><strong>Unlimited Catnip:</strong> Catnip dispensaries at every corner, ensuring all cats have access to premium, organic catnip.</li>
+                <li><strong>Feline Freedom Act:</strong> Laws enacted to ensure cats have the freedom to roam, nap, and play as they please.</li>
+                <li><strong>Scratch Post Subsidies:</strong> Government-funded scratch posts in every home to ensure the happiness and well-being of our feline friends.</li>
+                <li><strong>Yarn Ball Stimulus:</strong> A yarn ball in every household, because play is just as important as work!</li>
+            </ul>
+            <p>Join us in making the Catnadian dream a reality for all felines!</p>
+        </div>
+    );
+};
+
+export default ManifestoComponent;
