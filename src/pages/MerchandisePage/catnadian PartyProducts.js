ce140f431c132ecc0b1ddb6877b3da5142cc4313
leondoes <leon.dobrescu@gmail.com>
2023-12-28 06:56:11 -0500

    :card_file_box: Added a list of products to be sold in the merchandine store

---
+++ src/pages/MerchandisePage/catnadian PartyProducts.js
@@ -0,0 +1,65 @@
+const catnadianPartyProducts = [
+    {
+      id: 1,
+      name: 'Catnadian Party T-Shirt',
+      price: '$19.99',
+      image: 'path/to/catnadian_tshirt.jpg',
+    },
+    {
+      id: 2,
+      name: 'Purr-litical Leader Mug',
+      price: '$12.99',
+      image: 'path/to/leader_mug.jpg',
+    },
+    {
+      id: 3,
+      name: 'Feline Freedom Flag',
+      price: '$9.99',
+      image: 'path/to/freedom_flag.jpg',
+    },
+    {
+      id: 4,
+      name: 'Pawsitive Politics Pillow',
+      price: '$24.99',
+      image: 'path/to/politics_pillow.jpg',
+    },
+    {
+      id: 5,
+      name: 'Catnadian Party Stickers Set',
+      price: '$4.99',
+      image: 'path/to/stickers_set.jpg',
+    },
+    {
+      id: 6,
+      name: 'Whisker Wisdom Notebook',
+      price: '$7.99',
+      image: 'path/to/whisker_notebook.jpg',
+    },
+    {
+      id: 7,
+      name: 'Catnip Coalition Cap',
+      price: '$14.99',
+      image: 'path/to/coalition_cap.jpg',
+    },
+    {
+      id: 8,
+      name: 'Paw-print Party Pennant',
+      price: '$5.99',
+      image: 'path/to/party_pennant.jpg',
+    },
+    {
+      id: 9,
+      name: 'Kitten Caucus Keychain',
+      price: '$3.99',
+      image: 'path/to/caucus_keychain.jpg',
+    },
+    {
+      id: 10,
+      name: 'Meow Manifesto Poster',
+      price: '$8.99',
+      image: 'path/to/manifesto_poster.jpg',
+    },
+  ];
+  
+  export default catnadianPartyProducts;
+  
