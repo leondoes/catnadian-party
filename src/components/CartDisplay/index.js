9d06bd37550e4bef7809277426869424fd1a116a
leondoes <leon.dobrescu@gmail.com>
2024-01-06 23:14:29 -0500

    :sparkles: Added cart to merchandise page

---
+++ src/components/CartDisplay/index.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { CartContext } from './CartContext';
+
+const CartDisplay = () => {
+  const { cartItems } = useContext(CartContext);
+
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0);
+
+  return (
+    <div>
+      <h2>Cart</h2>
+      {cartItems.map((item, index) => (
+        <div key={index}>
+          <img src={item.image} alt={item.name} />
+          <p>{item.name}</p>
+          <p>${item.price}</p>
+        </div>
+      ))}
+      <div>Total: ${totalPrice}</div>
+    </div>
+  );
+};
+
+export default CartDisplay;
--- src/components/ProductGrid/index.js
+++ src/components/ProductGrid/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../CartContext';
 
 const ProductGrid = ({ products }) => {
+  const { addToCart } = useContext(CartContext);
+
   return (
     <div className="product-grid">
       {products.map(product => (
@@ -8,6 +11,7 @@ const ProductGrid = ({ products }) => {
           <img src={product.image} alt={product.name} />
           <h3>{product.name}</h3>
           <p>{product.price}</p>
+          <button onClick={() => addToCart(product)}>Add to Cart</button>
         </div>
       ))}
     </div>
