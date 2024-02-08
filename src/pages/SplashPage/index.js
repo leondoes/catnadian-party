1a73919e5d52b53ab01980b96eb0f12baa420447
leondoes <leon.dobrescu@gmail.com>
2024-02-08 10:32:53 -0500

    :white_check_mark: Testing delay on meta tag

--- src/pages/SplashPage/index.js
+++ src/pages/SplashPage/index.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import img from "../../assets/logo.png";
 import { SplashPicture, PageContainer, Title } from "./styled";
 
 const SplashPage = () => {
+  useEffect(() => {
+    // Function to dynamically insert the meta tag
+    const insertMetaTag = () => {
+      const metaTag = document.createElement("meta");
+      metaTag.name = "apple-itunes-app";
+      metaTag.content = "app-id=6456223179";
+      document.head.appendChild(metaTag);
+    };
+    const timer = setTimeout(insertMetaTag, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
       <PageContainer>
