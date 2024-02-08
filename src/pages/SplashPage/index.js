0d0032a6ee33b0b279654bcc434614cd005587fc
leondoes <leon.dobrescu@gmail.com>
2024-02-08 10:50:20 -0500

    :white_check_mark: Testing react-helmet to change meta tag after delay

--- src/pages/SplashPage/index.js
+++ src/pages/SplashPage/index.js
@@ -1,22 +1,26 @@
-import React, { useEffect } from "react";
-import img from "../../assets/logo.png";
-import { SplashPicture, PageContainer, Title } from "./styled";
+import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import img from '../../assets/logo.png';
+import { SplashPicture, PageContainer, Title } from './styled';
 
 const SplashPage = () => {
+  const [showMeta, setShowMeta] = useState(false);
+
   useEffect(() => {
-    // Function to dynamically insert the meta tag
-    const insertMetaTag = () => {
-      const metaTag = document.createElement("meta");
-      metaTag.name = "apple-itunes-app";
-      metaTag.content = "app-id=6456223179";
-      document.head.appendChild(metaTag);
-    };
-    const timer = setTimeout(insertMetaTag, 5000);
+    // Set a delay of 5000 milliseconds (5 seconds) to update the state
+    const timer = setTimeout(() => setShowMeta(true), 5000);
+
+    // Cleanup function to clear the timer if the component unmounts
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <div>
+      {showMeta && (
+        <Helmet>
+          <meta name="apple-itunes-app" content="app-id=6456223179" />
+        </Helmet>
+      )}
       <PageContainer>
         <Title>The Catnadian Party of Canada</Title>
         <SplashPicture>
