39faa0b720e6272f926866c82bb3b24e17496bf0
leondoes <leon.dobrescu@gmail.com>
2024-02-08 11:00:11 -0500

    :white_check_mark: react-helmet-async test on splash page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
-import App from './App'; // Your main App component
+import App from "./App"; // Your main App component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
       <Routes>
-        <Route path="/*" element={<App />} />
+        <Route
+          path="/*"
+          element={
+            <HelmetProvider>
+              <App />
+            </HelmetProvider>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>
