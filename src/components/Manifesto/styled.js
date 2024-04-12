fd91dd47279b4653c37e9cd8a5500bcf7dc9e476
leondoes <leon.dobrescu@gmail.com>
2024-04-12 05:58:13 -0400

    :lipstick: added font size to menifesto

--- src/components/Manifesto/styled.js
+++ src/components/Manifesto/styled.js
@@ -38,6 +38,7 @@ export const ListItem = styled.li`
   margin: 10px 0;
   padding: 10px;
   background-color: #e6e6fa; // Lavender background
+  font-size: 20px;
   border-radius: 5px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   transition: transform 0.3s ease, box-shadow 0.3s ease;
