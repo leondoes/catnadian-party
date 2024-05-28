29f1e9239fb7c89f273738bf3f7deb7ea0e58bf5
leondoes <leon.dobrescu@gmail.com>
2024-05-27 23:20:07 -0400

    :sparkles: Added product reviews to product page

--- src/components/ProductGrid/styled.js
+++ src/components/ProductGrid/styled.js
@@ -29,16 +29,19 @@ export const Image = styled.img`
   max-width: 100%;
   height: auto;
   border-radius: 5px;
+  object-fit: cover;
 `;
 
 export const Name = styled.h3`
   color: #333;
   margin: 10px 0;
+  text-align: center;
 `;
 
 export const Price = styled.p`
   color: #666;
   margin-bottom: 15px;
+  font-size: 18px;
 `;
 
 export const Button = styled.button`
@@ -49,6 +52,7 @@ export const Button = styled.button`
   color: white;
   cursor: pointer;
   transition: background-color 0.2s ease;
+  margin-top: 10px;
 
   &:hover {
     background-color: #0056b3;
@@ -59,16 +63,35 @@ export const Description = styled.p`
   color: #555;
   font-size: 14px;
   text-align: center;
+  margin: 10px 0;
 `;
 
 export const FeatureItem = styled.div`
   color: #555;
   font-size: 14px;
-  text-align: center;
+  text-align: left;
+  margin: 5px 0;
 `;
 
 export const FeatureList = styled.div`
   color: #555;
   font-size: 14px;
-  text-align: center;
+  text-align: left;
+  margin: 10px 0;
+`;
+
+export const Reviews = styled.div`
+  margin-top: 20px;
+`;
+
+export const Review = styled.div`
+  background-color: #f1f1f1;
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+`;
+
+export const Rating = styled.div`
+  font-weight: bold;
+  margin-bottom: 5px;
 `;
